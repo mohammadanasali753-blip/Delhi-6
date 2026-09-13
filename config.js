c90f5f1c2618548c7d98595f1c3547c375f96ab6
mohammadanasali753-blip <mohammadanasali753@gmail.com>
2026-09-13 21:05:01 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.DELHI6_CONFIG = {
+  SUPABASE_URL: "https://qxktximeddpttcjkaumo.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_mUL6OaOwJ649Xi68uQOcLw_9g0fM8mM",
+  ADMIN_PASSWORD: "Bilal@882664"
+};
